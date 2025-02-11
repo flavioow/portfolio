d5a8c2e7ef8df80f23ff479eeed0d6af5a913d15
Flávio Henrique <flaviohps8@gmail.com>
2025-02-10 22:26:44 -0300

    :lipstick: Feat: Content remaster
    Redesigned summary and added the main image of the about me section

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,4 +1,5 @@
 import Navbar from "@/components/Navbar"
+import Image from "next/image"
 import { getTranslations } from "next-intl/server"
 
 export default async function Home() {
@@ -7,19 +8,38 @@ export default async function Home() {
     return (
         <div>
             <Navbar />
-            <h1>
-                Flávi.<span className="font-calligraffitti">oow</span>
-            </h1>
-            <h2>{t("about.slogan")}</h2>
-            <p>{t("about.description")}</p>
+            <header className="m-8 grid grid-cols-[250px_minmax(300px,750px)] gap-12 justify-center">
+                <div className="relative w-[250px] h-[600px]">
+                    <Image
+                        src="/assets/profile-low.png"
+                        alt="Loading..."
+                        fill
+                        priority
+                        className="object-cover blur-sm"
+                    />
+                    <Image
+                        src="/assets/profile-high.png"
+                        alt="Profile picture"
+                        fill
+                        loading="lazy"
+                        className="object-cover transition-opacity duration-1000 ease-in-out"
+                    />
+                </div>
 
-            <button type="button">
-                <p>CV</p>
-            </button>
-
-            <button type="button">
-                <p>GitHub</p>
-            </button>
+                <div>
+                    <h1>
+                        Flávi.<span className="font-calligraffitti">oow</span>
+                    </h1>
+                    <h2>{t("about.slogan")}</h2>
+                    <p>{t("about.description")}</p>
+                    <button type="button">
+                        <p>CV</p>
+                    </button>
+                    <button type="button">
+                        <p>GitHub</p>
+                    </button>
+                </div>
+            </header>
         </div>
     )
 }
