9afd807271cbad75e354febf55a21b8e3b15847b
Flávio Henrique <flaviohps8@gmail.com>
2026-05-08 14:56:46 -0300

    :recycle: Refactor: Updating system
    I'll change the UI, logics and routes

--- src/components/layout/InfiniteScroll/index.tsx
+++
@@ -1,7 +0,0 @@
-import InfiniteScrollItem from "./InfiniteScrollItem"
-import InfiniteScrollRoot from "./InfiniteScrollRoot"
-
-export const InfiniteScroll = {
-    Root: InfiniteScrollRoot,
-    Item: InfiniteScrollItem,
-}
--- src/locales/server.ts
+++
@@ -1,6 +0,0 @@
-import { createI18nServer } from "next-international/server"
-
-export const { getI18n, getScopedI18n, getStaticParams } = createI18nServer({
-    en: () => import("./messages/en"),
-    br: () => import("./messages/pt-br"),
-})
