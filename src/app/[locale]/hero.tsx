cb841f944953bbf6e1a57c3eb446f1648fafe378
Flávio Henrique <flaviohps8@gmail.com>
2026-05-20 14:12:31 -0300

    :lipstick: Feat: Improve hero UI

---
+++ src/app/[locale]/hero.tsx
@@ -0,0 +1,49 @@
+import { useTranslations } from "next-intl"
+import { HeroEffect } from "@/components/hero-scene/hero-effect"
+import RotatingText from "@/components/rotating-text"
+
+export default function Hero() {
+  const t = useTranslations("home.hero")
+  const roles = [
+    t("roles.keyboardist"),
+    t("roles.designer"),
+    t("roles.developer"),
+    t("roles.brazilian"),
+    t("roles.handsome"),
+    t("roles.artist"),
+  ]
+
+  return (
+    <section
+      itemScope
+      itemType="http://schema.org/Person"
+      className="relative isolate h-dvh w-dvw overflow-hidden bg-radial from-background dark:from-muted from-28% to-muted-foreground dark:to-background">
+      <HeroEffect />
+
+      <div className="container-wrapper h-full content-end pb-8 pointer-events-none select-none text-background dark:text-foreground mix-blend-difference">
+        <h1 className="z-2 text-[clamp(4rem,26vw,26rem)] leading-[0.9] tracking-tighter font-title text-center absolute left-1/2 top-1/2 -translate-1/2">
+          Flavi.<span className="font-cursive">oow</span>
+        </h1>
+
+        <p className="container-breakout z-2 text-muted-foreground dark:text-foreground text-[clamp(1.4rem,2vw,2rem)] md:text-[clamp(1.6rem,3vw,3rem)]">
+          <span>{t("title")}&nbsp;</span>
+          <RotatingText
+            texts={roles}
+            mainClassName="overflow-hidden"
+            staggerFrom="last"
+            initial={{ y: "100%" }}
+            animate={{ y: 0 }}
+            exit={{ y: "-120%" }}
+            staggerDuration={0.02}
+            splitLevelClassName="overflow-hidden"
+            transition={{ type: "spring", damping: 30, stiffness: 400 }}
+            rotationInterval={3000}
+            splitBy="characters"
+            auto
+            loop
+          />
+        </p>
+      </div>
+    </section>
+  )
+}
--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -10,6 +10,7 @@ import { notFound } from "next/navigation"
 import { ThemeProvider } from "@/components/theme-provider"
 import { routing } from "@/i18n/routing"
 import { cn } from "@/lib/utils"
+import type { Metadata } from "next"
 
 const geist = Geist({
   subsets: ["latin"],
@@ -31,6 +32,61 @@ const calligraffitti = Calligraffitti({
   variable: "--font-calligraffitti",
   weight: ["400"],
 })
+
+export const metadata: Metadata = {
+  title:
+    "Flavi.oow - Flávio Henrique Perusin de Souza's Portfolio - Front-end Developer & Designer",
+  description:
+    "I'm Flávio Henrique, a front-end developer specializing in creating elegant and functional interfaces. I turn ideas into digital products that people love to use.",
+  keywords: [
+    "developer",
+    "front-end",
+    "seo",
+    "accessibility",
+    "responsiveness",
+    "typescript",
+    "node",
+    "react",
+    "nextjs",
+    "tailwindcss",
+    "prisma",
+    "postgresql",
+  ],
+  authors: [
+    {
+      name: "Flávio Henrique Perusin de Souza",
+      url: "https://github.com/flavioow",
+    },
+  ],
+  robots: "index, follow",
+  applicationName: "Flavi.oow",
+  manifest: "https://flavioow.vercel.app/manifest.webmanifest",
+  other: {
+    "google-site-verification": "MnOwS6_u35_3-3fShEOrsnUt17uLEEEA2UWCraW_mCM",
+  },
+  openGraph: {
+    title: "Flavi.oow",
+    description:
+      "I'm Flávio Henrique, a front-end developer specializing in creating elegant and functional interfaces. I turn ideas into digital products that people love to use.",
+    images: [
+      {
+        url: "https://flavioow.vercel.app/assets/opengraph.png",
+        width: 1200,
+        height: 630,
+        alt: "Preview",
+      },
+    ],
+    type: "website",
+    url: "https://flavioow.vercel.app/",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Flavi.oow",
+    description:
+      "I'm Flávio Henrique, a front-end developer specializing in creating elegant and functional interfaces. I turn ideas into digital products that people love to use.",
+    images: ["https://flavioow.vercel.app/assets/opengraph.png"],
+  },
+}
 
 export default async function RootLayout({
   children,
