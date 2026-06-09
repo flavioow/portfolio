26cd1734b040f042fd5348b6b89601e603aa19b8
Flávio Henrique <flaviohps8@gmail.com>
2026-06-09 20:12:39 -0300

    :sparkles: Feat: Add navbar menu for mobile devices

--- src/components/layout/navbar.tsx
+++ src/components/layout/navbar.tsx
@@ -4,6 +4,18 @@ import { TextRoll } from "../ui/skiper-ui/text-roll-navigation"
 import LanguageToggle from "./language-toggle"
 import { Logo } from "./logo"
 import { ThemeToggle } from "./theme-toggle"
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetDescription,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet"
+import { Menu } from "lucide-react"
+import { Button } from "../ui/button"
 
 const links = ["projects", "blog", "demos"] as const
 
@@ -37,10 +49,43 @@ export default function Navbar() {
             ))}
           </nav>
 
-          <div className="flex items-center justify-end gap-2">
+          <div className="hidden md:flex items-center justify-end gap-2">
             <LanguageToggle />
             <ThemeToggle />
           </div>
+
+          <Sheet>
+            <SheetTrigger className="md:hidden pointer-events-auto">
+              <Button size="icon" className="pointer-events-auto bg-input/20 hover:bg-input/20 dark:hover:bg-input/50 text-muted-foreground hover:text-background dark:hover:text-foreground transition-all duration-300">
+                <Menu
+                  aria-hidden="true"
+                  focusable="false"
+                />
+              </Button>
+            </SheetTrigger>
+            <SheetContent side="right">
+              <SheetHeader>
+                <SheetTitle />
+                <div className="flex items-center justify-center gap-2 mb-4">
+                  <LanguageToggle />
+                  <ThemeToggle />
+                </div>
+
+                <nav
+                  aria-label={tA11y("mainNavigation")}
+                  className="flex flex-col items-center justify-center gap-8">
+                  {links.map((link) => (
+                    <Link
+                      key={link}
+                      href={`/${link}`}
+                      className="uppercase text-sm text-muted-foreground hover:text-background dark:hover:text-foreground transition-colors duration-300">
+                      <TextRoll>{t(link)}</TextRoll>
+                    </Link>
+                  ))}
+                </nav>
+              </SheetHeader>
+            </SheetContent>
+          </Sheet>
         </div>
       </div>
     </header>
